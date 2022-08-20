195ed91a13b2ee21055181637ed2d7468b0d321c
JulianZea <cesarjulianzeabravo@gmail.com>
2022-08-20 01:08:16 -0500

    adding prototypes

--- monty.h
+++ monty.h
@@ -80,6 +80,13 @@ void swap(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
 int _isalpha(int c);
 
+/*advanced tasks*/
+
+void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+
 
 /* END */
 
