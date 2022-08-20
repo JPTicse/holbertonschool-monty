58045253a1f05ff480bc447703d7271f272aab92
JulianZea <cesarjulianzeabravo@gmail.com>
2022-08-20 00:01:35 -0500

    does nothing

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+ /** nop - does nothing
+ * @stack: the stack
+ * @line_number: instruction line number
+ * Return: nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	UNUSED(line_number);
+	UNUSED(stack);
+	argument[0] = "nothing";
+}
